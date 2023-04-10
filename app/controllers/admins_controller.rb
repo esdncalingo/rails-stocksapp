49cfc0cc6864f9965606b15e94a0d09e455009fb
esdncalingo <esdncalingo@gmail.com>
2023-04-10 14:13:30 +0800

    added:function admin can approve trader signups

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,20 @@ class AdminsController < ApplicationController
   before_action :require_user
 
   def index
-    @user = User.all
+    @user = User.where(status: "pending")
+  end
+
+  def activate_user
+    @user = User.where(status: "pending")
+    
+    respond_to do |format|
+      user = User.find_by(email: params[:email])
+      if user.update(status: "activated")
+        format.turbo_stream { render turbo_stream:
+          turbo_stream.update("usertable", partial: "admins/usertable", locals: { user: @user })
+        }
+      end
+    end
   end
 
 end
