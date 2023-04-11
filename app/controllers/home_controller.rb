b0bd86d8a63b83120587bdfc347b616f9cda9f1a
esdncalingo <esdncalingo@gmail.com>
2023-04-11 18:34:04 +0800

    added: pending view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,18 @@ class HomeController < ApplicationController
     response = Faraday.get('https://api.iex.cloud/v1/data/CORE/REF_DATA?token=pk_ed7475c0c153436587bd10b8f1da9916') 
     $stocks_master = JSON.parse(response.body)   
     # $stocks_master = response
+    # $stocks_master = JSON.parse(response.body)   
+    $stocks_master = response
+    
+    userauth = Authentication.find_by(token: session[:gen_token])
+    pending = userauth.user.status === "pending" ? true : false
+
+    if pending 
+      redirect_to pending_page_path
+    end
+  end
+
+  def pending
   end
 
   def sample 
