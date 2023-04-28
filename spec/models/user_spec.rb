2f517c7566128df1501cb5ed5fe328ef19b5e429
esdncalingo <esdncalingo@gmail.com>
2023-04-28 15:38:28 +0800

    added: test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe User, type: :model do
     expect(Authentication.login({:username => "danielc", :password => "12345678"})).to be_valid
   end
 
+  it 'can edit account' do
+    User.create_user({:username => "danielc", :password => "12345678", :email => "daniel@email.com"})
+    user = User.find_by(email: "daniel@email.com")
+    expect(User.signup({:id => user.id, :email => "daniel@email.com", :fname => "daniel"}))
+  end
+
+  it 'admin can create new account' do
+    expect(User.admin_create_user({:email => "testperson@email.com"}))
+  end
+
+  it 'admin can edit trader account details' do
+    User.create_user({:username => "danielc", :password => "12345678", :email => "daniel@email.com"})
+    expect(User.patch({
+      email: "daniel@email.com",
+      fname: "daniel",
+      mname: "nav",
+      lname: "calingo"
+    }))
+  end
+
 end
