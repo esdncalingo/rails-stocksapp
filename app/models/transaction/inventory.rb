468302d301cc4bacaf6c23729c8358d54b987d2e
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2023-05-06 13:07:34 +0800

    +Reduce N+1 Queries

--- app/models/transaction/inventory.rb
+++ app/models/transaction/inventory.rb
@@ -1,7 +1,10 @@
 class Transaction
   class Inventory
     def self.stock_count(user_id,stock_code)
-      stock_count = Transaction.where("user_id = ? AND stock_code = ?", user_id, stock_code).sum(&:qty)        
+      # stock_count= Transaction.includes(:user).where("user.id = ? AND transaction.stock_code = ?", user_id, stock_code)
+      # stock_count = Transaction.where("user_id = ? AND stock_code = ?", user_id, stock_code).sum(&:qty)        
+      user = User.find(user_id)
+      stock_count = user.transactions.where("user_id = ? AND stock_code = ?", user_id, stock_code).sum(&:qty)
       stock_count 
     end
   end
--- app/models/transaction/portfolio.rb
+++ app/models/transaction/portfolio.rb
@@ -3,7 +3,8 @@ class Transaction
     def self.display(user_id)  
       stocks_transactions =  my_transactions(user_id)
       my_portfolios = stocks_transactions.map { |transaction|
-        stock_details(user_id, transaction["stock_code"])
+        # binding.pry
+        stock_details(user_id, transaction[0], transaction[1]) 
         # stock_details(transaction["user_id"], transaction["stock_code"])
       }
       my_portfolios   
@@ -12,10 +13,12 @@ class Transaction
     private
     def self.my_transactions(user_id)
       user = User.find(user_id)
-      user.transactions.distinct.select("stock_code").where.not(stock_code: nil)
+      # user.transactions.distinct.select("stock_code").where.not(stock_code: nil)      
+      user.transactions.group(:stock_code).select(:stock_code, :qty).where.not(stock_code: nil).sum(:qty)
+      
     end
 
-    def self.stock_details(user_id, stock_symbol)
+    def self.stock_details(user_id, stock_symbol, stock_qty)
      
       stock_portfolio = {
         "logo" => "",
@@ -31,7 +34,8 @@ class Transaction
       stock_portfolio["logo"] = details["logo"]
       stock_portfolio["code"] =  details["code"]
       stock_portfolio["name"] =  details["name"]    
-      stock_portfolio["qty"] = Transaction::Inventory.stock_count(user_id, stock_symbol)        
+      # stock_portfolio["qty"] = Transaction::Inventory.stock_count(user_id, stock_symbol)        
+      stock_portfolio["qty"] = stock_qty
       stock_portfolio["latest_price"] =  details["latest_price"]   
       stock_portfolio["change_percent"] =  details["change_percent"]
       
